f15c6f175997264a7284949f31985821fd963609
sawatkins <sw@sawatkins.com>
2024-08-20 23:55:16 -0700

    add create server script

--- main.tf
+++ main.tf
@@ -10,9 +10,17 @@ resource "aws_instance" "tf2_server" {
   vpc_security_group_ids = ["sg-00eb70ad5a661788f"]
 
   root_block_device {
-    volume_size = 35
+    volume_size = 20
     volume_type = "gp3"
   }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y docker htop
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              EOF
 
   tags = {
     Name = "upfast-tf2-server"
@@ -26,4 +34,8 @@ output "instance_id" {
 
 output "instance_public_ip" {
   value = aws_instance.tf2_server.public_ip
+}
+
+output "instance_public_dns" {
+  value = aws_instance.tf2_server.public_dns
 }
