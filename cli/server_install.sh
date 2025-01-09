3a9994c09b973cc07cc37b4d096f8b96396efd54
Simon <41589344+sawatkins@users.noreply.github.com>
2025-01-09 00:24:22 -0800

    clean up server install script

--- cli/server_install.sh
+++ cli/server_install.sh
@@ -1,60 +1,74 @@
 #!/bin/bash
 
-# This script installs a Team Fortress 2 server following this guide
-# https://wiki.teamfortress.com/wiki/Linux_dedicated_server
-# for Debian/Ubuntu Linux machines
+# Script to install a dedicated Team Fortress 2 Linux server
+# - Checks prerequisites
+# - Installs required packages
+# - Creates gameserver user
+# - Installs TF2 server files
+# - Downloads custom maps
 
 set -e
 
-check_prerequisites() {
-    echo "Checking prerequisites..."
-    if ! grep -qEi "(debian|ubuntu)" /etc/os-release; then
-        echo "This script is intended for Debian/Ubuntu systems only."
-        exit 1
-    fi
+# Make sure script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run as root."
+  exit 1
+fi
 
-    local required_space=15000000 # 15GB in KB
-    local available_space=$(df / | tail -1 | awk '{print $4}')
-    if [ "$available_space" -lt "$required_space" ]; then
-        echo "Not enough disk space. At least 15GB is required."
-        exit 1
-    fi
-}
+# Check prerequisites
+if ! grep -qx 'ID=debian' /etc/os-release; then
+    echo "This script is for Debian only."
+    exit 1
+fi
 
-# CHECK PREREQUISITES
-check_prerequisites
+required_space=15000000 # 15GB
+available_space=$(df / | tail -1 | awk '{print $4}')
+if [ "$available_space" -lt "$required_space" ]; then
+    echo "Not enough disk space. At least 15GB is required."
+    exit 1
+fi
 
-# INSTALL REQUIREMENTS
-echo "Setting up system requirements..."
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install -y lib32z1 libncurses5:i386 libbz2-1.0:i386 lib32gcc-s1 lib32stdc++6 libtinfo5:i386 libcurl3-gnutls:i386 libsdl2-2.0-0:i386
+# Install required packages
+dpkg --add-architecture i386
+apt-get update
+apt-get install -y \
+  lib32z1 \
+  libncurses5:i386 \
+  libbz2-1.0:i386 \
+  lib32gcc-s1 \
+  lib32stdc++6 \
+  libtinfo5:i386 \
+  libcurl3-gnutls:i386 \
+  libsdl2-2.0-0:i386 \
+  wget \
+  tar
 
-# SETUP USER & INSTALL STEAMCMD
-echo "Creating gameserver user and setting up SteamCMD..."
-sudo adduser --disabled-login --gecos "" gameserver
-sudo -u gameserver bash -c "
-    mkdir -p /home/gameserver/hlserver &&
-    cd /home/gameserver/hlserver &&
-    wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz &&
-    tar zxf steamcmd_linux.tar.gz &&
-    rm steamcmd_linux.tar.gz
-"
+# Create gameserver user
+adduser --disabled-password --shell /bin/bash --gecos "" gameserver
 
-# DOWNLOAD THE SERVER
-echo "Downloading Team Fortress 2 server..."
-sudo -u gameserver bash -c "
-    /home/gameserver/hlserver/steamcmd.sh +force_install_dir /home/gameserver/hlserver/tf2 +login anonymous +app_update 232250 +quit
-"
+# Install tf2 server as gameserver user
+su - gameserver <<'EOF'
+set -e
 
-# CREATE & UPDATE SERVER CONFIG
-echo "Setting up server configuration files..."
-sudo -u gameserver bash -c "
-    cd /home/gameserver/hlserver/tf2/tf/cfg &&
-    touch server.cfg motd.txt mapcycle.txt &&
-    echo 'Please configure the files server.cfg, motd.txt, mapcycle.txt'
-"
+mkdir -p ~/hlserver
+cd ~/hlserver
 
-# TODO: download custom servers from s3
+# download and install tf2 server files
+wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+tar -xzf steamcmd_linux.tar.gz
+rm steamcmd_linux.tar.gz
+./steamcmd.sh +login anonymous +force_install_dir ~/hlserver/tf2 +app_update 232250 validate +quit
 
-echo "Installation complete."
+# install custom maps
+mkdir -p ~/hlserver/tf2/tf/maps
+cd ~/hlserver/tf2/tf/maps
+wget https://upfast.tf/maps/surf_utopia_v3.bsp
+wget https://upfast.tf/maps/surf_pulse_v2.bsp
+
+# create tf2 config files
+cd ~/hlserver/tf2/tf/cfg
+touch server.cfg motd.txt mapcycle.txt
+echo "Please configure server.cfg, motd.txt, and mapcycle.txt as needed."
+EOF
+
+echo "Installation complete"
