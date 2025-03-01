982de2563154a8849ee85160006ed496ee8bd4ae
sawatkins <sw@sawatkins.com>
2025-03-01 07:58:29 +0000

    add rcon connect bash script

---
+++ cli/rcon_connect.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Script to connect to each server with rcon
+
+# check args
+if [[ $# -ne 1 || !( "$1" == "tf2_server_us" || "$1" == "tf2_server_eu") ]]; then
+    echo "Usage: rcon_connect.sh <tf2_server_us|tf2_server_eu>"
+    exit 1
+fi
+
+cd ~/rcon || exit 1
+
+if [ ! -f "rcon.yaml" ]; then
+    echo "rcon.yaml file doesn't exist"
+    exit 1
+fi
+
+./rcon -e "$1"
